45b1477538892ea148249dd4b1c5f3230322bf07
Kaizu-r <jljoloc@gmail.com>
2026-02-28 14:22:59 +0800

    added flat stat

--- Models/EchoBase.py
+++ Models/EchoBase.py
@@ -1,8 +1,10 @@
 class EchoBase:
-    def __init__(self, name: str, set : str, cost: int, main_stat : str, main_stat_value : int, substats : list):
+    def __init__(self, name: str, set : str, cost: int, main_stat : str, main_stat_value : int,, flat_stat : str, flat_stat_value: int, substats : list):
         self.name = name
         self.set = set
         self.cost = cost
         self.main_stat = main_stat
         self.main_stat_value = main_stat_value
         self.substats = substats
+        self.flat_stat = flat_stat
+        self.flat_stat_value = flat_stat_value
